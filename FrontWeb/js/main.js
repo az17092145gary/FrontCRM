b0e30f7e24ba67efba3bb4766fef12b2ebe5b66e
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-11-18 14:18:41 +0800

    url test

--- FrontWeb/js/main.js
+++ FrontWeb/js/main.js
@@ -1,6 +1,7 @@
 //var FrontDomainUrl = "http://127.0.0.1:5500/FrontWeb/";
 var FrontDomainUrl = "https://az17092145gary.github.io/FrontCRM/FrontWeb/";
-var BackDomainUrl = "https://localhost:7226/";
+//var BackDomainUrl = "https://localhost:7226/";
+var BackDomainUrl = "https://0530-124-218-106-67.ngrok-free.app/";
 Number.prototype.numberFormat = function(c, d, t){
     var n = this, 
         c = isNaN(c = Math.abs(c)) ? 2 : c, 
